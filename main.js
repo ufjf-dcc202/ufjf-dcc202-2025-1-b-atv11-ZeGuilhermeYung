b2830a7b31941060741c4ece070220434bb742cd
José Guilherme <jgyung@gmail.com>
2025-08-05 23:13:09 -0300

    Adicionando um ouvinte ao botão

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-import { getLista } from "./lista.js";
+import { getLista, limpaLista } from "./lista.js";
 
 const olItens = document.querySelector("#itens");
 const pEntrada = document.querySelector("#entrada");
 const btnAdicionar = document.querySelector("#adicionar");
 const btnLimpar = document.querySelector("#limpar");
+
+btnLimpar.addEventListener('click', limparItensDeLista);
+
+atualizarLista();
 
 function atualizarLista() {
     olItens.innerHTML = "";
@@ -13,4 +17,9 @@ function atualizarLista() {
         li.textContent = lista[i];
         olItens.appendChild(li);
     }
+}
+
+function limparItensDeLista() {
+    limpaLista();
+    atualizarLista();
 }
