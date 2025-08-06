c0a7530434b1a4f103972074395146b15bce4509
José Guilherme <jgyung@gmail.com>
2025-08-05 23:27:48 -0300

    Finalizando com os testes

--- lista.js
+++ lista.js
@@ -4,6 +4,16 @@ export function getLista() {
     return structuredClone(lista);
 }
 
+export function adicionaNaLista(item) {
+    lista.push(item);
+}
+
+export function removeDaLista(posicao) {
+    if (posicao >= 0 && posicao < lista.length) {
+        lista.splice(posicao, 1);
+    }
+}
+
 export function limpaLista() {
-    lista.splice(0);
+    lista.splice(0, lista.length);
 }
