cd253e0c97398cb17a53bfb9f73c5d4c0f3726ed
José Guilherme <jgyung@gmail.com>
2025-08-05 23:08:34 -0300

    Atualizando a lista no main.js

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
+import { getLista } from "./lista.js";
+
 const olItens = document.querySelector("#itens");
 const pEntrada = document.querySelector("#entrada");
 const btnAdicionar = document.querySelector("#adicionar");
 const btnLimpar = document.querySelector("#limpar");
+
+function atualizarLista() {
+    olItens.innerHTML = "";
+    let lista = getLista();
+    for (let i = 0; i < lista.length; i++) {
+        const li = document.createElement('li');
+        li.textContent = lista[i];
+        olItens.appendChild(li);
+    }
+}
